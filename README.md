1ebd9f6f1ef1734b3927079020e7062b653794a3
Mainyf <3080574183@qq.com>
2019-01-18 22:38:58 +0800

    init project.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kotlin Forge 
+
+Minecraft Forge 1.7.10 kotlin starter template
+
+```cmd
+git clone https://github.com/Mainyf/KotlinForge
+
+windows: gradlew setupDecompWorkspace
+Linux/Mac OS: ./gradlew setupDecompWorkspace
+
+idea: gradlew idea
+eclipse: gradlew eclipse
+```
---
+++ src/main/kotlin/com/example/examplemod/ExampleMod.kt
@@ -0,0 +1,22 @@
+package com.example.examplemod
+
+import net.minecraft.init.Blocks
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.Mod.EventHandler
+import cpw.mods.fml.common.event.FMLInitializationEvent
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+class ExampleMod {
+
+    companion object {
+        const val MODID = "examplemod"
+        const val VERSION = "1.0"
+    }
+
+    @EventHandler
+    fun init(event: FMLInitializationEvent) {
+        // some example code
+        println("DIRT BLOCK >> " + Blocks.dirt.unlocalizedName)
+    }
+
+}
